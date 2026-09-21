2fdfbd5062cebcb9953124009474a76d665b0127
Bella Teixeira <bellateixeiraetec@gmail.com>
2026-09-21 20:33:36 -0300

    Add files via upload

--- JavaScript/daltonismo.js
+++ JavaScript/daltonismo.js
@@ -9,3 +9,28 @@ function fecharOpcoesDaltonismo() {
 
     fundo.classList.remove("ativo");
 }
+
+function ativarFiltro(filtro) {
+    document.body.classList.remove(
+        "protanopia",
+        "deuteranopia",
+        "tritanopia",
+        "acromatopsia"
+    );
+
+    if (filtro !== "normal") {
+        document.body.classList.add(filtro);
+    }
+
+    localStorage.setItem("filtroDaltonismo", filtro);
+}
+
+function carregarFiltro() {
+    const filtro = localStorage.getItem("filtroDaltonismo");
+
+    if (filtro && filtro !== "normal") {
+        document.body.classList.add(filtro);
+    }
+}
+
+carregarFiltro();
